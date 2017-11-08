7bce5860cdbd3c8dd516dc531b3a00893fa40345
samiralajmovic <alajmovic.samir@gmail.com>
2017-11-08 22:24:31 +0100

    feat: Add subcommands build and lint

--- src/tasks.js
+++ src/tasks.js
@@ -1,40 +1,22 @@
-const child = require('child_process');
+const startLint = require('./lint.js');
 const path = require('path');
+const webpackTask = require('./webpack.js');
 
 const TASKS = Object.freeze({
   build: options => build(options),
-  dev: options => dev(options)
+  lint: ({ entry, watch }) => startLint({ entry, watch })
 });
 
 module.exports = TASKS;
 
-function dev({ template, port, entry, output }) {
-  console.log('dev');
-  const webpack = path.join(path.dirname(__filename), 'webpack.js');
+function build({ lint, template, entry, output, port, watch }) {
+  if (lint) {
+    startLint({ entry, watch });
+  }
 
-  child.execSync(
-    `node ${webpack} --watch --port ${port} --template ${template} --entry ${entry} --output ${output}`,
-    {
-      stdio: [0, 1, 2]
-    }
-  );
-}
-
-function build({ template, entry, output }) {
-  console.log('build');
-  const webpack = path.join(path.dirname(__filename), 'webpack.js');
-
-  child.execSync(
-    `node ${webpack} --template ${template} --entry ${entry} --output ${output}`,
-    {
-      stdio: [0, 1, 2]
-    }
-  );
-}
-
-function changeStartingTime(
-  { timer, updateStartingTime },
-  { target: { value: startingTime } }
-) {
-  updateStartingTime({ timer, startingTime });
+  if (watch) {
+    webpackTask.dev({ port, entry, template, output });
+  } else {
+    webpackTask.build({ entry, template, output, port, watch });
+  }
 }

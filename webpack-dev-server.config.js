7b10494a749f7f8e16bac9407897f62dbfbf1fd8
samiralajmovic <alajmovic.samir@gmail.com>
2017-11-19 14:36:34 +0100

    feat(dev-server): Only output errors and warnings

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -4,14 +4,28 @@ function config({ contentBase, port = 3000 }) {
   return {
     contentBase,
     port,
-    clientLogLevel: 'none', // 'info',
+    clientLogLevel: 'none',
     compress: true,
     filename: 'bundle.js',
     host: 'localhost',
     historyApiFallback: true,
     inline: true,
     publicPath: '/',
-    stats: { colors: true },
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: false,
+      warnings: true
+    },
     watchOptions: {
       aggregateTimeout: 0,
       poll: 500

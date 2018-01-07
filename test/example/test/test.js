55d97d5afd0f609a735246fee95a18d96ccda02d
samiralajmovic <alajmovic.samir@gmail.com>
2018-01-07 19:39:46 +0100

    chore: Add README's to each test folder

--- test/example/test/test.js
+++ test/example/test/test.js
@@ -1,45 +1,7 @@
-import puppeteer from 'puppeteer';
-import { catchErrorEvents, isPortFree, startTestServer } from './lib.js';
-import { settingsTest } from './functional-tests/settings.spec.js';
-
-const PORT = 1337;
-const BASE_URL = route => `http://localhost:${PORT}${route}`;
+import { functionalTests } from './functional-tests/index.js';
 
 main();
-
 async function main() {
-  let server;
-  try {
-    let portFree = await isPortFree(PORT);
-    if (portFree) {
-      server = startTestServer(PORT);
-    }
-  } catch (e) {
-    console.error(e);
-  }
-
-  /**
-   * Init Puppeteer
-   */
-  const browser = await puppeteer.launch({
-    headless: true,
-    slowMo: 0
-  });
-
-  /**
-   * Test
-   */
-  try {
-    await settingsTest({
-      browser,
-      BASE_URL,
-      catchErrorEvents
-    });
-  } catch (e) {
-    console.error(e);
-  }
-
-  // await page.waitFor(5000);
-  server ? server.close() : '';
-  await browser.close();
+  await unitTests();
+  await functionalTests();
 }

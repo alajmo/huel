ba30dee96edf50c9f1cf7a2b4ba89c753e5a8c09
samiralajmovic <alajmovic.samir@gmail.com>
2018-02-03 20:42:19 +0100

    feat: Add dependency check

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -1,3 +1,4 @@
+const Depcheck = require('depcheck');
 const PJV = require('package-json-validator').PJV;
 const bytes = require('bytes');
 const chalk = require('chalk');
@@ -9,7 +10,7 @@ const path = require('path');
 
 module.exports = startTest;
 
-function startTest({ pjv, size }) {
+function startTest({ pjv, size, depcheck, entry, output }) {
   if (pjv) {
     validatePackageJson();
   }
@@ -17,24 +18,97 @@ function startTest({ pjv, size }) {
   if (size) {
     sizeLimit(size);
   }
+
+  if (depcheck) {
+    depcheckTest({ entry, output });
+  }
 }
 
-function runTests(src) {
-  const babelNodePath = path.join(
-    '..',
-    '..',
-    'node_modules',
-    'babel-cli',
-    'bin',
-    'babel-node.js'
-  );
+function depcheckTest({ entry, output }) {
+  const resolveAliases = {
+    components: path.join(process.cwd(), entry, 'components'),
+    res: path.join(process.cwd(), entry, 'res'),
+    lib: path.join(process.cwd(), entry, 'lib'),
+    pages: path.join(process.cwd(), entry, 'pages')
+  };
 
-  child.exec(`${babelNodePath} ${src}`, (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    if (err !== null) {
-      console.log('exec error: ' + err);
+  const webpackAliasDetect = node => {
+    if (node.type === 'ImportDeclaration' && node.source && node.source.value) {
+      for (let key in resolveAliases) {
+        if (
+          node.source.value.startsWith(key + '/') ||
+          node.source.value === key
+        ) {
+          // console.log('node.source.value: ', node.source.value);
+          // console.log('resolveAliases[key]: ', resolveAliases[key]);
+          return [
+            path.resolve(
+              path.join(resolveAliases[key]),
+              '..',
+              node.source.value
+            )
+          ];
+        }
+      }
+
+      return [node.source.value];
     }
+    return [];
+  };
+
+  const options = {
+    ignoreBinPackage: true,
+    ignoreMatches: ['hal'],
+    ignoreDirs: [output],
+    detectors: [Depcheck.detector.requireCallExpression, webpackAliasDetect],
+    specials: [Depcheck.special.eslint, Depcheck.special.webpack]
+  };
+
+  Depcheck(path.join(process.cwd()), options, unused => {
+    let dependencyCheckPassed = true;
+
+    if (unused.dependencies.length > 0) {
+      dependencyCheckPassed = false;
+      console.log('Unused dependencies:');
+      unused.dependencies.forEach(dep => console.log(chalk.red(dep)));
+      console.log();
+    }
+    if (unused.devDependencies.length > 0) {
+      dependencyCheckPassed = false;
+      console.log('Unused devDependencies:');
+      unused.devDependencies.forEach(dep => console.log(chalk.red(dep)));
+      console.log();
+    }
+    if (Object.keys(unused.missing).length > 0) {
+      dependencyCheckPassed = false;
+      console.log('Missing dependencies:');
+      Object.keys(unused.missing).forEach(dep => console.log(chalk.red(dep)));
+      console.log();
+    }
+
+    if (Object.keys(unused.invalidFiles).length > 0) {
+      dependencyCheckPassed = false;
+      console.log('Invalid files:');
+      Object.keys(unused.invalidFiles).forEach(file =>
+        console.log(chalk.red(file))
+      );
+      console.log();
+    }
+
+    if (Object.keys(unused.invalidDirs).length > 0) {
+      dependencyCheckPassed = false;
+      console.log('Invalid directories:');
+      Object.keys(unused.invalidDirs).forEach(dir =>
+        console.log(chalk.red(dir))
+      );
+      console.log();
+    }
+
+    console.log(
+      dependencyCheckPassed
+        ? chalk.green('Dependency check passed!')
+        : chalk.red('Dependency check failed!')
+    );
   });
 }
 
@@ -77,6 +151,10 @@ function validatePackageJson() {
   }
 }
 
+/**
+ * @desc Checks size limit of files.
+ * @param {string|boolean} size If string then assumed to be a path to size-limit config file.
+ */
 function sizeLimit(size) {
   const sizeConfigPath =
     typeof size === 'string'
@@ -130,4 +208,23 @@ function sizeLimit(size) {
 
 function formatBytes(size) {
   return bytes.format(size, { unitSeparator: ' ' });
+}
+
+function runTests(src) {
+  const babelNodePath = path.join(
+    '..',
+    '..',
+    'node_modules',
+    'babel-cli',
+    'bin',
+    'babel-node.js'
+  );
+
+  child.exec(`${babelNodePath} ${src}`, (err, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (err !== null) {
+      console.log('exec error: ' + err);
+    }
+  });
 }

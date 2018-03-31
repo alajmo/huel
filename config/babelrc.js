4484a70c0d630f037a253f60ce0b11a5849b8520
samiralajmovic <alajmovic.samir@gmail.com>
2018-03-31 12:53:47 +0200

    fix: add polyfill for async await in babel

--- config/babelrc.js
+++ config/babelrc.js
@@ -1,12 +1,28 @@
-module.exports = 
-{
+module.exports = {
   babelrc: false,
   cacheDirectory: true,
-  presets: [require.resolve('babel-preset-env')],
+  presets: [
+    [
+      require.resolve('babel-preset-env'),
+      {
+        targets: {
+          node: 'current',
+          browsers: ['last 2 versions']
+        }
+      }
+    ]
+  ],
   retainLines: true,
   plugins: [
     require.resolve('babel-plugin-transform-object-rest-spread'),
-    [require.resolve('babel-plugin-transform-react-jsx'), { pragma: 'h' }]
+    [require.resolve('babel-plugin-transform-react-jsx'), { pragma: 'h' }],
+    [
+      require.resolve('babel-plugin-transform-runtime'),
+      {
+        polyfill: false,
+        regenerator: true
+      }
+    ]
   ],
   compact: false,
   comments: true,
